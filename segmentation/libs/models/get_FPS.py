60ed17ff1e0b5100c6e509cd0137768fc3715e12
sunruifei <73230372+sunruifei@users.noreply.github.com>
2024-11-15 11:04:30 +0800

    Add files via upload

---
+++ segmentation/libs/models/get_FPS.py
@@ -0,0 +1,10 @@
+from .models import DeepLabV1
+ 
+ 
+model = DeepLabV1("/root/autodl-tmp/nsrom11/segmentation/data/models/voc12/deeplabv2_resnet101_msc/train_our/checkpoint_final.pth")
+model.predict(source="/root/autodl-tmp/nsrom11/data/test",save=True,save_conf=True,save_txt=True,name='output')
+ 
+#source��ΪҪԤ���ͼƬ���ݼ��ĵ�·��
+#save=TrueΪ����Ԥ����
+#save_conf=TrueΪ����������Ϣ
+#save_txt=TrueΪ����txt���������yolov8����ͼƬ��Ԥ�ⲻ������ʱ��������txt�ļ�
